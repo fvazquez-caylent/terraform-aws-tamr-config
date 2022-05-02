f365f79d4d99ded5d3b9e8204938450f79ca545d
fvazquez-caylent <85187881+fvazquez-caylent@users.noreply.github.com>
2022-05-02 16:51:00 -0300

    CA-214 - Fixes examples to allow the use of terraform version 0.13.7. (#1)
    * Fixes examples to allow the use of terraform version 0.13.7.
    
    * Removes unneeded argument.

--- examples/ephemeral-spark/ec2-key-pair.tf
+++ examples/ephemeral-spark/ec2-key-pair.tf
@@ -1,3 +1,10 @@
+#################################################################################################################
+# This version has been patched to allow the use of terraform version 0.13.7, if you are using a newer
+# version we suggest going to the next major release.
+# This version is creating security groups using resource blocks instead of modules.
+# Internal ticket for reference is CA-214.
+#################################################################################################################
+
 # Create new EC2 key pair
 resource "tls_private_key" "emr_private_key" {
   algorithm = "RSA"
